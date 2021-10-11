4f10b3b2c54c3f8eab950f4145d170fac8b07a9b
LAPTOP-R8KTH405\ducht <ducht.personalemail@gmail.com>
2021-10-11 09:12:21 +0700

    Test Firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+// libs
+import { initializeApp } from "firebase/app";
+import * as firestore from "firebase/firestore/lite";
 // others
 import "./App.scss";
 
+const { getFirestore, collection, getDocs } = firestore;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBXbSdsAqGAx7m8CjQzIdkpqVdWd3otl0",
+  authDomain: "testdatabase-185de.firebaseapp.com",
+  projectId: "testdatabase-185de",
+  storageBucket: "testdatabase-185de.appspot.com",
+  messagingSenderId: "716426653291",
+  appId: "1:716426653291:web:9760b374bb4df0f7fd0b2e",
+  measurementId: "G-21GVDV0XYZ"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const db = getFirestore(firebase);
+async function getCities() {
+  const citiesCol = collection(db, "test-collection");
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
+
 function App() {
+  getCities().then(res => {
+    console.log({ res });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
